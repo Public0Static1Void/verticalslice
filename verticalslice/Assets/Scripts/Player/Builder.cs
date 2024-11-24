d5b320e5bb38bd3127d33b40d93953f0d23581ce
Dani <dangp2003@gmail.com>
2024-11-24 12:35:55 +0100

    Colocación de estructuras mejorada
    Ahora se ajusta automáticamente y manualmenta la altura de la estructura al moverla, la distancia al jugador también se puede modificar manualmente

--- verticalslice/Assets/Scripts/Player/Builder.cs
+++ verticalslice/Assets/Scripts/Player/Builder.cs
@@ -16,15 +16,64 @@ public class Builder : MonoBehaviour
 
     public Buildings curr_build = Buildings.LAST_NO_USE;
     GameObject curr_build_ob;
+
+    [Header("Stats")]
+    [SerializeField] private float offset_from_ground;
+    [SerializeField] private float distance_from_player;
+    private float max_distance_from_player, max_offset_from_ground;
+
+    public bool relocate = false;
+    public bool right_click_pressed = false;
+    private float delta = 0;
+    public enum DistanceMode { UP, FORWARD }
+    public DistanceMode distance_mode = DistanceMode.FORWARD;
     void Start()
     {
-        if (buildings.Count >= (int)Buildings.LAST_NO_USE)
+        if (buildings.Count > (int)Buildings.LAST_NO_USE)
         {
             Debug.LogWarning("There are more buildings in the list that in the enum");
         }
 
         CreateGrid(build_menu.transform.GetChild(0).gameObject);
         build_menu.SetActive(false);
+
+        max_distance_from_player = distance_from_player;
+        max_offset_from_ground = offset_from_ground;
+    }
+
+    void Update()
+    {
+        if (right_click_pressed)
+        {
+            delta += Time.deltaTime;
+            if (delta > 1)
+            {
+                distance_mode = DistanceMode.UP;
+            }
+            else
+            {
+                distance_mode = DistanceMode.FORWARD;
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (relocate)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(curr_build_ob.transform.position, -transform.up, out hit))
+            {
+                if (hit.distance != offset_from_ground)
+                {
+                    float new_y = Mathf.Lerp(curr_build_ob.transform.position.y, hit.point.y + offset_from_ground, Time.fixedDeltaTime * 3);
+                    curr_build_ob.transform.position = new Vector3(curr_build_ob.transform.position.x, new_y, curr_build_ob.transform.position.z);
+                }
+            }
+            curr_build_ob.transform.position = new Vector3(
+                transform.position.x + Camera.main.transform.forward.x * distance_from_player, 
+                curr_build_ob.transform.position.y, transform.position.z + Camera.main.transform.forward.z * distance_from_player);
+        }
     }
 
     public void OpenMenu(InputAction.CallbackContext con)
@@ -81,14 +130,17 @@ public class Builder : MonoBehaviour
         open_menu = false;
 
         Debug.Log("Building " + build);
-        curr_build_ob = Instantiate(buildings[(int)build], transform.position + (transform.forward * 5), Quaternion.identity);
-        curr_build_ob.transform.SetParent(Camera.main.transform);
+        curr_build_ob = Instantiate(buildings[(int)build], transform.position + (transform.forward * distance_from_player), Quaternion.identity);
+        
         curr_build = build;
+        relocate = true;
     }
     public void PlaceBuilding(InputAction.CallbackContext con)
     {
         if (con.performed && curr_build != Buildings.LAST_NO_USE)
         {
+            relocate = false;
+
             switch (curr_build)
             {
                 case Buildings.CONVEYOR:
@@ -99,10 +151,44 @@ public class Builder : MonoBehaviour
                     break;
             }
 
-            curr_build_ob.transform.SetParent(null);
             curr_build_ob.transform.rotation = buildings[(int)curr_build].transform.rotation;
             curr_build_ob = null;
             curr_build = Buildings.LAST_NO_USE;
+        }
+    }
+
+    public void ChangeLength(InputAction.CallbackContext con)
+    {
+        if (con.performed && curr_build != Buildings.LAST_NO_USE)
+        {
+            Vector2 input = con.ReadValue<Vector2>();
+            Debug.Log(input);
+            switch (distance_mode)
+            {
+                case DistanceMode.UP:
+                    offset_from_ground += input.y / 100;
+                    offset_from_ground = Mathf.Clamp(offset_from_ground, 0.5f, max_offset_from_ground);
+                    break;
+                case DistanceMode.FORWARD:
+                    distance_from_player += input.y / 100;
+                    distance_from_player = Mathf.Clamp(distance_from_player, 1, max_distance_from_player);
+                    break;
+            }
+        }
+    }
+
+    public void ChangeModeOrDelete(InputAction.CallbackContext con)
+    {
+        if (curr_build == Buildings.LAST_NO_USE) return;
+
+        if (con.performed)
+        {
+            right_click_pressed = true;
+        }
+        if (con.canceled)
+        {
+            delta = 0;
+            right_click_pressed = false;
         }
     }
 }
